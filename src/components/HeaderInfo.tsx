eab6bf441dac69dc8528829215d72457464d89f1
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 15:23:30 -0600

    Add HeaderInfo component to display IP information and location details. This new component presents the IP address along with the corresponding country emoji and flag, enhancing the application's capability to showcase user-specific data.

---
+++ src/components/HeaderInfo.tsx
@@ -0,0 +1,28 @@
+interface HeaderInfoProps {
+    ip: string;
+    location: {
+        country_emoji: string;
+        country_flag: string;
+        country_name: string;
+    };
+}
+
+export default function HeaderInfo({ ip, location }: HeaderInfoProps) {
+    return (
+        <header className="flex items-center justify-between border-b border-gray-400 pb-6 w-full max-w-7xl" >
+            <div>
+                <h1 className="text-4xl font-bold text-center text-white">
+                    Información de la IP {ip}
+                </h1>
+            </div>
+            <div className="flex items-center gap-3 justify-center">
+                <span className="text-4xl text-white flex items-center justify-center">{location.country_emoji}</span>
+                <img
+                    src={location.country_flag}
+                    alt={location.country_name}
+                    className=" object-contain rounded shadow"
+                />
+            </div>
+        </header >
+    )
+}   
