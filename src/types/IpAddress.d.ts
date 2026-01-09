df7c8513c2f8629dd85714bc897eea8b49ea8798
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 14:25:46 -0600

    Add IpAddress type definition for IP location data structure. This new type outlines the format for IP-related location, country metadata, and currency details, facilitating better data handling in the application.

---
+++ src/types/IpAddress.d.ts
@@ -0,0 +1,27 @@
+export type IpLocationData = {
+    ip: string;
+    location: {
+        continent_name: string;
+        country_name: string;
+        country_name_official: string;
+        country_capital: string;
+        state_prov: string;
+        city: string;
+        zipcode: string;
+        latitude: string;
+        longitude: string;
+        country_flag: string;
+        country_emoji: string;
+        is_eu: boolean;
+    };
+    country_metadata: {
+        calling_code: string;
+        tld: string;
+        languages: string[];
+    };
+    currency: {
+        code: string;
+        name: string;
+        symbol: string;
+    };
+};
