7b4e677d9f56423f711fa7eccf617d838dc7ef24
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 10:26:03 -0600

    Refactor App component to utilize PagesRoutes for improved routing structure. This change simplifies the component hierarchy by removing individual components like NavBar, Etiqueta, Hero, and FondoWave, enhancing maintainability and organization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import NavBar from "./components/NavBar"
-import FondoWave from "./components/FondoWave"
-import Etiqueta from "./components/Etiqueta"
-import { FaSearch } from "react-icons/fa";
-import Hero from "./pages/Hero"
-
+import PagesRoutes from "./routes/pages.routes";
 function App() {
 
   return (
-    <div className="w-full min-h-screen animated-bg flex pt-10 flex-col relative">
-      <NavBar />
-
-      <Etiqueta text="Encontrar la ubicación de una IP" icon={<FaSearch />} />
-
-      <Hero />
-
-      <FondoWave />
-    </div>
+    <PagesRoutes />
   )
 }
 
