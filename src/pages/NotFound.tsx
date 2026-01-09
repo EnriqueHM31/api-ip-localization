032557c5b0ed02bb6d2a96e00c088c60ff9c5158
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 10:33:39 -0600

    Enhance NotFound component with improved styling and navigation. Updated the 404 message to be more prominent, added a link to return to the homepage, and included a FondoWave component for visual appeal.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,17 @@
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import FondoWave from "../components/FondoWave";
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center py-5 gap-8">
-            <h1 className="text-white text-6xl font-bold mt-20">404</h1>
-            <p className="text-white text-2xl font-bold">La página que buscas no existe</p>
+        <div className="flex flex-col items-center justify-center py-5 gap-8 min-h-[70vh]">
+            <h1 className="text-white text-9xl font-bold animate-bounce ">404</h1>
+            <p className="text-white text-4xl font-bold animate-bounce">La página que buscas no existe</p>
+            <Link to="/" className="bg-blue-700 text-white px-4 py-2 rounded-md font-bold flex items-center gap-2 cursor-pointer hover:bg-white transition-all duration-300 hover:text-blue-950 ease-in">
+                <FaHome />
+                <p>Volver al inicio</p>
+            </Link>
+
+            <FondoWave />
         </div>
     )
 }
