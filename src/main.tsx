1826bfa989e00a548afea938634d24f97814008d
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 10:26:08 -0600

    Integrate BrowserRouter into main.tsx for enhanced routing capabilities. This update replaces StrictMode with BrowserRouter, allowing for improved navigation and routing management within the application.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import '@fontsource-variable/open-sans/wght.css';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
