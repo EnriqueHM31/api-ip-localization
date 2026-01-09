6db33e5d35021c7b4ab7e211e0a4a4cb4702ca76
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-09 10:25:57 -0600

    Enhance Hero component layout by adding NavBar, Etiqueta, and FondoWave. This update improves the user interface for IP address search functionality and organizes the component structure.

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,14 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+import Etiqueta from "../components/Etiqueta";
+import FondoWave from "../components/FondoWave";
 import FormIpAddress from "../components/IpAddress";
+import NavBar from "../components/NavBar";
 
 export default function Hero() {
     return (
-        <section className="flex flex-col items-center justify-center py-5 gap-8">
-            <h1 className="text-white text-6xl font-bold mt-20">Search IP Address</h1>
 
-            <div className="flex items-center flex-col gap-5 w-full max-w-4xl">
-                <FormIpAddress />
-            </div>
-        </section>
+        <div className="w-full min-h-screen animated-bg flex pt-10 flex-col relative">
+            <NavBar />
+
+            <Etiqueta text="Encontrar la ubicación de una IP" icon={<FaSearch />} />
+
+            <section className="flex flex-col items-center justify-center py-5 gap-8">
+                <h1 className="text-white text-6xl font-bold mt-20">Search IP Address</h1>
+
+                <div className="flex items-center flex-col gap-5 w-full max-w-4xl">
+                    <FormIpAddress />
+                </div>
+            </section>
+
+            <FondoWave />
+        </div>
     )
 
 }
