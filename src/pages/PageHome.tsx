6fecba1192d9c57dd2035fe22cb76e62ab8e33db
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:17:48 -0600

    style: update PageHome component for improved layout and responsive design, including adjustments to heading size and removal of FondoWave component

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,23 +1,21 @@
 import { FaSearch } from "react-icons/fa";
 import Etiqueta from "../components/Atomos/Etiqueta";
-import FondoWave from "../components/Atomos/FondoWave";
 import FormIpAddress from "../components/sections/IpAddress";
 
 export default function Hero() {
     return (
-        <div className="max-w-7xl w-full flex flex-col items-center justify-center mx-auto">
+        <div className=" md:max-w-7xl w-full flex flex-col items-center justify-center mx-auto">
 
             <Etiqueta text="Encontrar la ubicación de una IP" icon={<FaSearch />} />
 
-            <section className="flex flex-col items-center justify-center py-5 gap-8   w-full ">
-                <h1 className="text-white text-6xl font-bold mt-5">Search IP Address</h1>
+            <section className="flex flex-col items-center justify-center py-5 gap-8 w-full  mt-5 mb-50">
+                <h1 className="text-white text-3xl md:text-6xl font-bold mt-5 text-center">Search IP Address</h1>
 
                 <div className="flex items-center flex-col gap-5 w-full ">
                     <FormIpAddress />
                 </div>
             </section>
 
-            <FondoWave />
         </div>
     )
 
