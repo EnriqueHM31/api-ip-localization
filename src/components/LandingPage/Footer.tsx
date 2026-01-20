dea9a047dc3ff01afcbc94356be5eda226fb3bff
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 22:21:27 -0600

    style: enhance Footer component layout for improved responsiveness and alignment, including adjustments to text positioning and flex properties

--- src/components/LandingPage/Footer.tsx
+++ src/components/LandingPage/Footer.tsx
@@ -23,10 +23,10 @@ export default function Footer() {
             <footer className="bg-white text-gray-900 z-50">
                 <div className="max-w-7xl mx-auto">
 
-                    <div className="mx-auto flex  justify-between items-start max-w-7xl gap-10 px-6 py-3 ">
+                    <div className="mx-auto flex md:flex-row flex-col  justify-between items-start max-w-7xl gap-10 px-6 py-3 ">
                         {/* BRAND */}
                         <div className="space-y-4 flex-1">
-                            <div className="flex items-center gap-3">
+                            <div className="flex items-center justify-center md:justify-start gap-3">
                                 <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-950 text-white font-bold">
                                     HM
                                 </div>
@@ -35,11 +35,11 @@ export default function Footer() {
                                 </span>
                             </div>
 
-                            <p className="text-sm text-blue-950">
+                            <p className="text-sm text-blue-950 text-center md:text-left">
                                 Diseño y desarrollo de experiencias web modernas, rápidas y escalables.
                             </p>
 
-                            <ul className="space-y-2 text-sm flex flex-col gap-2">
+                            <ul className="space-y-2 text-sm flex flex-col gap-2 items-center md:items-start">
                                 <li className="flex items-center gap-2">
                                     <FaEnvelope className="text-blue-950" />
                                     lxxxxxxxx@gmail.com
@@ -58,7 +58,7 @@ export default function Footer() {
 
 
                         {/* NEWSLETTER */}
-                        <div className="flex-1">
+                        <div className="flex-1 flex flex-col items-center md:items-start">
                             <h4 className="mb-4 text-sm font-semibold  tracking-wider text-black">
                                 Enviar comentarios
                             </h4>
@@ -85,7 +85,7 @@ export default function Footer() {
                     </div>
 
                     {/* BOTTOM */}
-                    <div className="border-t-2 border-blue-950 py-4 text-center text-xs text-blue-950">
+                    <div className="border-t-2 border-blue-950 py-4 text-center text-xs text-blue-950 mx-5 md:mx-0">
                         © {new Date().getFullYear()} HM Projects. Todos los derechos reservados.
                     </div>
                 </div>
