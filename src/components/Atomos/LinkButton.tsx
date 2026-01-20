b23bf005aa0c85c04ffecf846c716cfa3a7b4586
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:16:53 -0600

    fix: update import path for PropsLinkButton to maintain consistency in capitalization

--- src/components/Atomos/LinkButton.tsx
+++ src/components/Atomos/LinkButton.tsx
@@ -1,9 +1,9 @@
-import type { PropsLinkButton } from "../../types/components";
+import type { PropsLinkButton } from "../../types/Components";
 
 export default function LinkButton({ text, icon }: PropsLinkButton) {
     return (
         <a href="https://github.com/EnriqueHM31?tab=repositories" target="_blank" className="bg-white text-blue-950 px-4 py-2 rounded-md font-bold flex items-center gap-2 cursor-pointer hover:bg-blue-600 transition-all duration-300 hover:text-white ease-in">
-            <p>{text}</p>
+            <p className="hidden md:block">{text}</p>
             {icon}
         </a>
     )
