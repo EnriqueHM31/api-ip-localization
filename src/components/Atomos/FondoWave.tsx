3d6baa3721722334075696963549b197e163bdb7
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 22:21:15 -0600

    style: adjust FondoWave component positioning for improved layout and responsiveness

--- src/components/Atomos/FondoWave.tsx
+++ src/components/Atomos/FondoWave.tsx
@@ -1,6 +1,6 @@
 export default function FondoWave() {
     return (
-        <div className="flex absolute   bottom-36 left-0 items-end w-full h-full pointer-events-none">
+        <div className="flex absolute -top-120 md:top-auto md:bottom-36 left-0 items-end w-full h-full pointer-events-none">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#fff" d="M0,96L60,96C120,96,240,96,360,122.7C480,149,600,203,720,202.7C840,203,960,149,1080,117.3C1200,85,1320,75,1380,69.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
             </svg>
