aaa474ee6b458b12557f914b41b972bfcfe603eb
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:17:44 -0600

    refactor: update FormIpAddress component for improved styling and functionality, including responsive design adjustments and form submission handling

--- src/components/sections/IpAddress.tsx
+++ src/components/sections/IpAddress.tsx
@@ -1,14 +1,19 @@
 import { useLocalizacion } from "../../hooks/Localizacion";
-import type { PropsFormIpAddress } from "../../types/components";
+import type { PropsFormIpAddress } from "../../types/Components";
 
-export default function FormIpAddress({ styleForm = "flex-col max-w-4xl ", styleInput = "w-28 h-14 text-3xl my-5", stylePoint = "text-4xl h-14", styleButton = "px-8 py-2 text-2xl" }: PropsFormIpAddress) {
+export default function FormIpAddress({ styleForm = "flex-col max-w-4xl ", styleInput = "w-16 h-10 md:w-28 md:h-14 text-lg md:text-3xl my-5 ", stylePoint = "text-4xl h-10 md:h-14", styleButton = "px-8 py-2 text-lg md:text-xl" }: PropsFormIpAddress) {
 
     const { values, inputsRef, handleChange, handleKeyDown, ipValue, handleBuscar } = useLocalizacion();
 
     return (
-        <form className={`flex items-center flex-col gap-5 w-full ${styleForm}`} >
-
-            <div className="flex gap-2 justify-end  ">
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleBuscar();
+            }}
+            className={`flex w-full flex-col items-center gap-5 ${styleForm}`}
+        >
+            <div className="flex justify-end gap-2">
                 {values.map((val, index) => (
                     <div key={index} className="flex items-center gap-2">
                         <input
@@ -17,30 +22,31 @@ export default function FormIpAddress({ styleForm = "flex-col max-w-4xl ", style
                             }}
                             type="text"
                             inputMode="numeric"
-                            id={`input-${index}`}
                             maxLength={3}
                             value={val}
                             onChange={(e) => handleChange(index, e.target.value)}
                             onKeyDown={(e) => handleKeyDown(index, e)}
-                            className={` ${styleInput} text-blue-950  bg-white  text-center font-semibold border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500`}
+                            className={`${styleInput} bg-white text-center font-semibold text-blue-950 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500`}
                         />
+
                         {index < 3 && (
-                            <span className={`${stylePoint} font-bold select-none text-white flex items-end`}>
+                            <span className={`${stylePoint} font-bold select-none text-white`}>
                                 .
                             </span>
                         )}
                     </div>
                 ))}
-
             </div>
+
             <input type="hidden" value={ipValue} />
+
             <button
-                type="button"
-                onClick={handleBuscar}
-                className={`${styleButton} bg-blue-600 cursor-pointer text-white rounded-2xl font-bold  hover:bg-white transition-all duration-300 hover:text-blue-950 ease-in`}
+                type="submit"
+                className={`${styleButton} bg-blue-600 cursor-pointer rounded-2xl font-bold text-white transition-all duration-300 hover:bg-white hover:text-blue-950`}
             >
                 Buscar
             </button>
-        </form >
+        </form>
+
     );
 }
