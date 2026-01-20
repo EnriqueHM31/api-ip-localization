0c232fd0a69a714d2d45488894977e245ed604f1
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:16:42 -0600

    style: update Etiqueta component for improved layout and text responsiveness

--- src/components/Atomos/Etiqueta.tsx
+++ src/components/Atomos/Etiqueta.tsx
@@ -2,11 +2,11 @@ import type { PropsEtiqueta } from "../../types/Components";
 
 export default function Etiqueta({ text, icon }: PropsEtiqueta) {
     return (
-        <div className="bg-white text-blue-950 font-bold rounded-2xl flex items-center gap-2 pe-4 max-w-fit mx-auto my-5">
+        <div className="bg-white text-blue-950 font-bold rounded-2xl flex items-center gap-2 pe-4 max-w-fit mx-auto md:my-5 mt-14">
             <div className="bg-blue-600 text-white px-4 py-2 rounded-tl-2xl rounded-bl-2xl">
                 {icon}
             </div>
-            <p>{text}</p>
+            <p className="text-sm md:text-base">{text}</p>
         </div>
     )
 }
