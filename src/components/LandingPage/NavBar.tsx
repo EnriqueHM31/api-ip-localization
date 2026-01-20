89bac7967ed1664305903db00868c8f77155d096
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 21:17:35 -0600

    style: update NavBar component for improved layout and responsive design

--- src/components/LandingPage/NavBar.tsx
+++ src/components/LandingPage/NavBar.tsx
@@ -9,18 +9,18 @@ export default function NavBar() {
 
     const location = useLocation();
     return (
-        <nav className="flex justify-between items-center py-1  w-full max-w-7xl mx-auto">
+        <nav className="flex justify-between items-center py-1  w-full md:max-w-7xl mx-auto px-3 md:px-0">
             <Link to="/" className="flex items-center gap-4">
-                <picture className="size-12 bg-white rounded-full p-2 object-cover flex items-center justify-center">
+                <picture className="size-8 md:size-12 bg-white rounded-full p-1 md:p-2 object-cover flex items-center justify-center">
                     <img src={ICONO} alt="logo" className=" object-cover" />
                 </picture>
-                <h1 className="text-white text-2xl font-bold">Search IP Address</h1>
+                <h1 className="text-white text-lg md:text-2xl font-bold">Search IP Address</h1>
             </Link>
             {
                 location.pathname === "/" ?
                     <LinkButton text="Portafolio de proyectos HM" icon={<FaGithub />} /> :
                     <section className="flex items-center justify-center  w-full max-w-fit ">
-                        <FormIpAddress styleForm="flex-row justify-end" styleInput="w-15 h-9" stylePoint="text-xl h-7" styleButton="px-4 py-1 text-lg" />
+                        <FormIpAddress styleForm="flex-row justify-end" styleInput="w-15 h-9" stylePoint="text-xl h-7" styleButton="px-4 py-2 text-sm" />
                     </section>
             }
         </nav>
