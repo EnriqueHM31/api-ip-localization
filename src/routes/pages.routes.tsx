8537e95dd6715aea420edb6ef50462350275189a
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-21 12:51:42 -0600

    feat: implement lazy loading for routes in PagesRoutes component to enhance performance and user experience

--- src/routes/pages.routes.tsx
+++ src/routes/pages.routes.tsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
-import PageHome from "../pages/PageHome";
-import Page404 from "../pages/Page404";
-import PageIpInformacion from '../pages/PageIpInformacion';
+import { Suspense, lazy } from 'react';
+import Loading from '../components/Atomos/Loading';
+
+const PageHome = lazy(() => import('../pages/PageHome'));
+const Page404 = lazy(() => import('../pages/Page404'));
+const PageIpInformacion = lazy(() => import('../pages/PageIpInformacion'));
+
 
 export default function PagesRoutes() {
     return (
-        <Routes>
-            <Route path="/" element={<PageHome />} />
-            <Route path=":ip" element={<PageIpInformacion />} />
-            <Route path="*" element={<Page404 />} />
-        </Routes>
+        <Suspense fallback={<div className='min-h-screen'>
+            <Loading text="Cargando..." />
+        </div>}>
+
+            <Routes>
+
+                <Route path="/" element={<PageHome />} />
+                <Route path=":ip" element={<PageIpInformacion />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+
+        </Suspense>
     )
 }
