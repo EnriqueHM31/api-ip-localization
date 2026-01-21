177db3ecfbad11b152c6e3dd9f532b36cddb0698
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-20 22:16:50 -0600

    refactor: update normalizeError function to accept error string instead of IpLocationData, improving error handling consistency

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,45 +1,44 @@
-import type { IpLocationData } from "../types/IpAddress";
 import { TypesErrors } from "../constants";
 import { VerificarError } from "./index";
 
 
-export const normalizeError = (data: IpLocationData) => {
-    if (VerificarError({ data, codigo: TypesErrors.Bogon.codigo, palabraIncluida: TypesErrors.Bogon.palabraIncluida })) {
+export const normalizeError = (error: string) => {
+    if (VerificarError({ error, codigo: TypesErrors.Bogon.codigo, palabraIncluida: TypesErrors.Bogon.palabraIncluida })) {
         return {
             type: TypesErrors.Bogon.type,
             message: TypesErrors.Bogon.message,
         };
     }
 
-    if (VerificarError({ data, codigo: TypesErrors.Dominio.codigo, palabraIncluida: TypesErrors.Dominio.palabraIncluida })) {
+    if (VerificarError({ error, codigo: TypesErrors.Dominio.codigo, palabraIncluida: TypesErrors.Dominio.palabraIncluida })) {
         return {
             type: TypesErrors.Dominio.type,
             message: TypesErrors.Dominio.message,
         };
     }
 
-    if (VerificarError({ data, codigo: TypesErrors.Requerida.codigo, palabraIncluida: TypesErrors.Requerida.palabraIncluida })) {
+    if (VerificarError({ error, codigo: TypesErrors.Requerida.codigo, palabraIncluida: TypesErrors.Requerida.palabraIncluida })) {
         return {
             type: TypesErrors.Requerida.type,
             message: TypesErrors.Requerida.message,
         };
     }
 
-    if (VerificarError({ data, codigo: TypesErrors.Invalido.codigo, palabraIncluida: TypesErrors.Invalido.palabraIncluida })) {
+    if (VerificarError({ error, codigo: TypesErrors.Invalido.codigo, palabraIncluida: TypesErrors.Invalido.palabraIncluida })) {
         return {
             type: TypesErrors.Invalido.type,
             message: TypesErrors.Invalido.message,
         };
     }
 
-    if (VerificarError({ data, codigo: TypesErrors.Servicio.codigo, palabraIncluida: TypesErrors.Servicio.palabraIncluida })) {
+    if (VerificarError({ error, codigo: TypesErrors.Servicio.codigo, palabraIncluida: TypesErrors.Servicio.palabraIncluida })) {
         return {
             type: TypesErrors.Servicio.type,
             message: TypesErrors.Servicio.message,
         };
     }
 
-    if (VerificarError({ data, codigo: TypesErrors.Servidor.codigo, palabraIncluida: TypesErrors.Servidor.palabraIncluida })) {
+    if (VerificarError({ error, codigo: TypesErrors.Servidor.codigo, palabraIncluida: TypesErrors.Servidor.palabraIncluida })) {
         return {
             type: TypesErrors.Servidor.type,
             message: TypesErrors.Servidor.message,
